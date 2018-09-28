089ae34bbacd9337b7f2c77428d699d41e2f4c78
Chris Bojemski <chris.bojemski@gmail.com>
2018-09-28 11:56:38 -0400

    Gainz 💪

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
 class PowersController < ApplicationController
+
+  before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
   end
 end

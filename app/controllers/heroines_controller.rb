9269553f0b421af66b55a642e81a4230412c3d6f
yapjinai <jinai.jyap@gmail.com>
2018-09-28 11:49:30 -0400

    crushed it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,60 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @heroine = Heroine.new
+    @power_options = Power.all.map { |p| [p.name, p.id] }
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
   def index
     @heroines = Heroine.all
+    @q = params[:powers]
   end
+
+  def show
+  end
+
+  def edit
+    @power_options = Power.all.map { |p| [p.name, p.id] }
+  end
+
+  def update
+    @heroine.update(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to edit_heroine_path
+    end
+  end
+
+  def destroy
+    @heroine.destroy
+    redirect_to heroines_path
+  end
+
+  def search
+
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+  validates :name, presence: true
+  validates :super_name, {
+    presence: true,
+    uniqueness: true
+  }
+
+  belongs_to :power
 end

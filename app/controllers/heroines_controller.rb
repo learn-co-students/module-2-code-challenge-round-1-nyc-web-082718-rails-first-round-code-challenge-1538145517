4bc87ba42ad125bdf5d7fd0010583b0107120cd2
github username <github email address>
2018-09-28 12:01:31 -0400

    crushed it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,48 @@
 class HeroinesController < ApplicationController
+
+  before_action :find_heroine, only: [:show, :update, :destroy, :edit]
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+    redirect_to @heroine
+  else
+    flash[:error] = @heroine.errors.full_messages
+    redirect_to new_heroine_path
+  end
+  end
+
+  # def edit
+  # end
+  #
+  # def update
+  #   @heroine.update(heroine_params)
+  #   redirect_to @heroine
+  # end
+
+  # def destroy
+  #   @heroine.destroy
+  #   redirect_to heroines_path
+  # end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end

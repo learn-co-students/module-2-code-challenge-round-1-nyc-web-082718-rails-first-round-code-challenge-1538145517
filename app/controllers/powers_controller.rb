29a00184c5e4b88aaa844811a4ee993ab40d1126
github username <github email address>
2018-09-28 12:05:52 -0400

    App with Edit Form and Delete Option

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,56 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.create(power_params)
+    if @power.valid?
+      flash[:notice] = "Power Created!"
+      redirect_to @power
+    else
+      flash[:error] = @power.errors.full_messages
+      redirect_to new_power_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @power.update(power_params)
+    if @power.valid?
+      flash[:notice] = "Power Updated!"
+      redirect_to @power
+    else
+      flash[:error] = @power.errors.full_messages
+      redirect_to edit_power_path
+    end
+  end
+
+  def destroy
+    @power.destroy
+    redirect_to powers_path
+  end
+
+private
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
   end
 end

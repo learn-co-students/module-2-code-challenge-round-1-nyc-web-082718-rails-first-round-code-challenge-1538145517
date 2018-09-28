e75a7f6c60a97082712bd1330546cdd6fe3baf51
MarSchlaefer <mschlaefer00@gmail.com>
2018-09-28 11:59:47 -0400

    Crushed it

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :super_name, uniqueness: true
+
+  # def self.search(search)
+  #   where("power LIKE ?", "%#{search}%")
+  # end
+
 end
